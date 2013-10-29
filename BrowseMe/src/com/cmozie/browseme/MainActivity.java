8479ddfb91b6778f617e0aa40c0575acd6a8a539
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-29 15:26:06 -0400

    Created custom button/added class to handle javascript

--- BrowseMe/src/com/cmozie/browseme/MainActivity.java
+++ BrowseMe/src/com/cmozie/browseme/MainActivity.java
@@ -1,8 +1,14 @@
 package com.cmozie.browseme;
 
+import java.net.URI;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		WebView browser = (WebView) findViewById(R.id.webViewer);
+		
+		WebSettings settings = browser.getSettings();
+		settings.setJavaScriptEnabled(true);
+		browser.addJavascriptInterface(new WebAppInterface(this), "Android");
+		
+		Uri url = Uri.parse("http://www.google.com");
+		Intent webSite = new Intent(Intent.ACTION_VIEW, url);
+		
+		
+		
+		
+		
 	}
 
 	@Override
