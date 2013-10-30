5101a9dcf81c0b6f0c84249dbee41511d5beaa3d
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-30 09:02:32 -0400

    Added basic functionality. back/forward/search. Going to work on history.

--- BrowseMe/gen/com/cmozie/browseme/R.java
+++ BrowseMe/gen/com/cmozie/browseme/R.java
@@ -26,11 +26,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080005;
+        public static final int backButn=0x7f080003;
+        public static final int browseButn=0x7f080000;
         public static final int editText1=0x7f080001;
+        public static final int forwardButn=0x7f080004;
+        public static final int historyButn=0x7f080005;
         public static final int webViewer=0x7f080002;
     }
     public static final class layout {
--- BrowseMe/src/com/cmozie/browseme/MainActivity.java
+++ BrowseMe/src/com/cmozie/browseme/MainActivity.java
@@ -6,31 +6,93 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public EditText website;
+	public static WebView browser;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		WebView browser = (WebView) findViewById(R.id.webViewer);
+		final WebView browser = (WebView) findViewById(R.id.webViewer);
 		
 		WebSettings settings = browser.getSettings();
 		settings.setJavaScriptEnabled(true);
 		browser.addJavascriptInterface(new WebAppInterface(this), "Android");
 		
-		Uri url = Uri.parse("http://www.google.com");
-		Intent webSite = new Intent(Intent.ACTION_VIEW, url);
+		//Uri url = Uri.parse("http://www.google.com");
+		//Intent webSite = new Intent(Intent.ACTION_VIEW, url);
+		
+		browser.loadUrl("http://www.google.com");
+		Button search = (Button) findViewById(R.id.browseButn);
+		Button back = (Button) findViewById(R.id.backButn);
+		Button forward = (Button) findViewById(R.id.forwardButn);
+		Button history = (Button) findViewById(R.id.historyButn);
+		website = (EditText) findViewById(R.id.editText1);
 		
 		
+		//conditions for buttons
+		
+			search.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					String url = website.getText().toString();
+					browser.loadUrl(url);
+					Log.i("url", url);
+					
+					
+				}
+			});
 		
 		
+			back.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					if (browser.canGoBack()) {
+						browser.goBack();
+					}
+					
+				}
+			});
 		
-	}
+		
+			
+			forward.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					if (browser.canGoForward()) {
+						browser.goForward();
+					}
+					
+				}
+			});
+		
+		
+			history.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+			
+		}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
