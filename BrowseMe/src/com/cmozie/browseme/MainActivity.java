b044de680974edb830d8addaf16b1a96fb478f5a
Cameron Mozie <camsw0rld14@gmail.com>
2013-10-31 20:35:17 -0400

    Final commit

--- BrowseMe/src/com/cmozie/browseme/MainActivity.java
+++ BrowseMe/src/com/cmozie/browseme/MainActivity.java
@@ -78,7 +78,10 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		        setContentView(R.layout.activity_main);
+
+		    
 		
 	
 		//setting intent to getIntent
