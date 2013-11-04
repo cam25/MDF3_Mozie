58c1cc672a1afb5a099d7030c758149db69009ca
Cameron Mozie <camsw0rld14@gmail.com>
2013-11-03 22:48:01 -0500

    Created layout with buttons

--- RecordMe/src/com/cmozie/recordme/MainActivity.java
+++ RecordMe/src/com/cmozie/recordme/MainActivity.java
@@ -3,6 +3,10 @@ package com.cmozie.recordme;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,39 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button startRecorder = (Button) findViewById(R.id.openRecorder);
+		Button playVid = (Button) findViewById(R.id.playButn);
+		Button stopVid = (Button) findViewById(R.id.stopButn);
+		VideoView theView = (VideoView)findViewById(R.id.videoView1);
+		
+		startRecorder.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		playVid.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		stopVid.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 	}
 
 	@Override
