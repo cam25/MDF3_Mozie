498e387cd8e2d86eafefd5df940bc1f13cdcd15a
Cameron Mozie <camsw0rld14@gmail.com>
2013-11-19 09:39:52 -0500

    Implementing Javascript/Trials

--- RegionZip/src/com/cmozie/regionzip/Feedback.java
+++ RegionZip/src/com/cmozie/regionzip/Feedback.java
@@ -3,27 +3,58 @@ package com.cmozie.regionzip;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-@SuppressLint("SetJavaScriptEnabled")
+@SuppressLint({ "SetJavaScriptEnabled", "JavascriptInterface" })
 public class Feedback extends Activity {
-
+	public static WebView theWebView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.feedback);
 		
-		WebView myWebView = (WebView) findViewById(R.id.webView);
-		myWebView.loadUrl("file:///android_asset/feedback.html");
-		WebSettings webSettings = myWebView.getSettings();
+		WebView theWebView = (WebView) findViewById(R.id.webView);
+		theWebView.loadUrl("file:///android_asset/feedback.html");
+	
+		JavaScriptInterface jsInterface = new JavaScriptInterface(this);
+		
+		theWebView.addJavascriptInterface(jsInterface, "CallJava");
+		
+		WebSettings webSettings = theWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
+		
+		
 		ActionBar actionBar = getActionBar();
 	    actionBar.setDisplayHomeAsUpEnabled(true);
+	
+	    
 	}
+	
+	public class JavaScriptInterface {
+	    Context context;
+
+	  
+	    JavaScriptInterface(Context c) {
+	        context = c;
+	    }
+	    public void getJSText(){
+	    	AlertDialog.Builder myDialog
+	        = new AlertDialog.Builder(Feedback.this);
+	        myDialog.setTitle("Test!");
+	        myDialog.setMessage("Hopefully this loads!");
+	        myDialog.setPositiveButton("ON", null);
+	        myDialog.show();
+	    }
+
+	  
+	}
+	 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
