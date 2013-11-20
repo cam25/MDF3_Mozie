30b68f202bcba894953dfaa225acd68bc3379907
Cameron Mozie <camsw0rld14@gmail.com>
2013-11-20 10:21:59 -0500

    Added clear button for clearing text area(HTML)/added comments/cleanup

--- RegionZip/src/com/cmozie/regionzip/Feedback.java
+++ RegionZip/src/com/cmozie/regionzip/Feedback.java
@@ -3,24 +3,23 @@ package com.cmozie.regionzip;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.ConsoleMessage;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 
+@SuppressLint("SetJavaScriptEnabled")
 public class Feedback extends Activity {
 	public static WebView theWebView;
+	
+	//javascript interface
 public class JavaScriptInterface {
 		
 	    Context context;
@@ -35,9 +34,8 @@ public class JavaScriptInterface {
 	    
 	    @JavascriptInterface
 	    public void getJSText(String textValue, String rateValue) {
-	    	//toasts
-	        Toast.makeText(context, rateValue, Toast.LENGTH_SHORT).show();
-	        Toast.makeText(context, textValue, Toast.LENGTH_SHORT).show();
+	    	//toast for sending feedback
+	        Toast.makeText(context, "Thanks for your Feedback!", Toast.LENGTH_SHORT).show();
 	        
 	        //email intent 
 	        Intent emailIntent = new Intent(Intent.ACTION_SEND);
@@ -45,8 +43,8 @@ public class JavaScriptInterface {
 	        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "cmozie@ymail.com" });
 	        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "RegionZip Feedback");
 	        emailIntent.putExtra(Intent.EXTRA_TEXT, "You rated the app: " + rateValue + " \nComments:" + textValue);
-	        startActivity(Intent.createChooser(emailIntent, "Send Feedback:"));
-	      
+	        startActivity(Intent.createChooser(emailIntent, "Send Feedback Via Email:"));
+	        //sets image on screen 
 	        theWebView.loadUrl("file:///android_asset/good.png");
 	    }
 	  
@@ -59,42 +57,33 @@ public class JavaScriptInterface {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.feedback);
 		
+		//sets the webview
 		WebView theWebView = (WebView) findViewById(R.id.webView);
 		theWebView.loadUrl("file:///android_asset/feedback2.html");
 	
-		//final JavaScriptInterface jsInterface = new JavaScriptInterface(this);
-		
+		//adds the javascript interface to the webview for access to between html/webview via javascript
 		theWebView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
 		
 		WebSettings webSettings = theWebView.getSettings();
+		
+		//enables javascript
 		webSettings.setJavaScriptEnabled(true);
 		
-		
+		//enables actionbar
 		ActionBar actionBar = getActionBar();
 	    actionBar.setDisplayHomeAsUpEnabled(true);
 	    
 	    
 	}
-	
-	
-	//shows javascript errors
-	public boolean onConsoleMessage(ConsoleMessage cm) 
-    {
-        Log.d("ShowMote", cm.message() + " -- From line "
-                             + cm.lineNumber() + " of "
-                             + cm.sourceId() );
-        return true;
-    }
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item){
 		switch (item.getItemId()) {
-		
-		//returns to main activity
+		//transitions to each page and back via click of item in action bar
 		case android.R.id.home:
 			Intent homeIntent = new Intent(this, MainActivity.class);
 			  homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			  startActivity(homeIntent);
-
 			break;
 		case R.id.sendFeedback:
 			Intent feedBackIntent = new Intent(this, Feedback.class);
@@ -102,8 +91,7 @@ public class JavaScriptInterface {
 			  break;
 		case R.id.aboutPage:
 			Intent aboutIntent = new Intent(this, About.class);
-			  startActivity(aboutIntent);
-			
+			  startActivity(aboutIntent);		
 		break;
 		default:
 			break;
